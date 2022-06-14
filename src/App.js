c46cda528d2833eb11df9e267dd1e4b7522830be
Ahmed Ragap <Ahmed-Ragap>
2022-06-14 02:28:14 +0200

    first uploading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { increase, decrease } from './commnAction';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+render(){ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <h1>React redux Counter example</h1>
+ <div className='preview'>{this.props.count}</div>
+  <button onClick={this.props.increase}>counter +</button>
+  <button onClick={this.props.decrease}>counter -</button>
+  <br/>
+  <br />
+  <br />
+<div className='info'>
+  <h3>tools i used</h3>
+   <span>createStore // dispatch // action creator // connect function // reducer // state // props</span>
+</div>
+
     </div>
   );
 }
+}
+//state
+function mapState(state){
+  return{
+    count:state.count,
+    name:state.name,
+    add:state.add
+  }
+}
 
-export default App;
+//action 
+function mapDispatch(dispatch) {
+  return{
+    increase:() => dispatch(increase),
+    decrease:() => dispatch(decrease)
+  }
+}
+export default connect(mapState, mapDispatch) (App)
---
+++ src/reducers/Reducer.js
@@ -0,0 +1,20 @@
+const initState = 0
+
+const reducer = (state=initState, action) =>{
+
+     if(action.type =='INCREASE'){
+            return{
+                count:state.count + 1
+            }
+        
+    } else if (action.type =='DECREASE'){
+        return{
+ count:state.count - 1
+        }
+    }
+    return{
+        count:0
+    }
+}
+
+export default reducer
